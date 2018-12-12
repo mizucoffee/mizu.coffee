9b0a7cbbd5897f872a48a2925a281ceb17f44e95
Ritsuki Goto <kawakawaritsuki.dev@gmail.com>
2018-12-13 01:32:00 +0900

    [delete] old version build

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
-for i in `git tag`
-do
-  git checkout $i
-  yarn
-  npx gulp
-  mv dest/ $i
-done
-
-git checkout master
+# for i in `git tag`
+# do
+#   git checkout $i
+#   yarn
+#   npx gulp
+#   mv dest/ $i
+# done
+#
+# git checkout master
 yarn
 npx gulp
 
-for i in `git tag`
-do
-  mv $i dest/
-done
+# for i in `git tag`
+# do
+#   mv $i dest/
+# done
